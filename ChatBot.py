bcf172fed7f133ccf49e9588186ed1d666ff77b1
shadow-ghost-fanthom <carloseduardomotanogueira@gmail.com>
2026-01-23 15:11:35 -0400

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -3,14 +3,13 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import google.generativeai as genai
 
-# CHAVE API
+# API KEY
 genai.configure(api_key="AIzaSyAxPOxuc5LY5cywfCGqncixqCPkn5budXE")
 
 app = Flask(__name__)
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
-    # .strip() remove qualquer espaço invisível que venha na mensagem
     pergunta = request.values.get('Body', '').strip()
     resp = MessagingResponse()
     
@@ -18,18 +17,16 @@ def webhook():
         return str(resp)
 
     try:
-        # Forçamos o uso do modelo flash
         model = genai.GenerativeModel('gemini-1.5-flash')
         response = model.generate_content(pergunta)
         resposta_texto = response.text
     except Exception as e:
-        print(f"ERRO: {e}")
-        resposta_texto = "Desculpe, tive um erro na conexão com a IA."
+        print(f"ERRO NO GEMINI: {e}")
+        resposta_texto = "Ops, tive um problema aqui. Tente novamente!"
 
     resp.message(resposta_texto)
     return str(resp)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-
     app.run(host='0.0.0.0', port=port)
